863375bab2216627038e5006b11e672b1a3157af
akshara-a <akshararajan1726@gmail.com>
2022-10-30 00:17:16 +0530

    Update

--- db/connect.php
+++ db/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "ffe";
+    $servername = "sql12.freemysqlhosting.net";
+    $username = "$NAME";
+    $password = "$PASS";
+    $dbname = "$DB";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
